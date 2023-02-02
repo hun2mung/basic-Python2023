dac15fc3a112c0dfae02c3a3305946d463f2cf1f
hun2mung <ldh5444@naver.com>
2023-02-02 17:28:50 +0900

    Day04 2023-02-02
    Day04

--- Day04/code22_person.py
+++ Day04/code22_person.py
@@ -1,13 +1,26 @@
 # 클래스 생성
 
-class Person:
-    name = '익명'
+class Person:           # 클래스 선언
+    name = '익명'       # 속성 변수
     height = ''
     gender = ''
     blood_type = 'A'
 
+    # 1. 생성자 추가(__init__)
+    # def __init__(self):
+    #     self.name = '홍길동'
+    #     self.height = '170'
+    #     self.gender = 'male'
+    #     self.blood_type = 'AB'
+
+    # 재정의(overriding) : 원하는 기능으로 변경
+    def __init__(self, name = '홍길동', height = 170, gender = 'male'): # 매개변수(파라미터) 기본값
+        self.name = name            # self.속성변수 = 매개변수
+        self.height = height
+        self.gender = gender      
+
     def walk(self):
-        print('걷습니다.')
+        print(f'{self.name}이(가) 걷습니다.')
 
     def run(self, option):
         if option == 'Fast':
@@ -17,14 +30,33 @@ class Person:
             print(f'{self.name}이(가) 천천히 뜁니다!!')
     
     def stop(self):
-        print('멈춥니다.')
+        print(f'{self.name}이(가) 멈춥니다.')
+
+    # 2. 생성자 외 매직메서드(함수) ex) __str__
+    def __str__(self) -> str:              # string값으로 return 받음
+        return f'출력 : 이름은 {self.name}, 성별은 {self.gender}'
     
-donghun = Person()  # 이렇게 만들어진 객체를 instance라 한다.
-donghun.name = '이동훈'
-donghun.height = '170'
-donghun.gender = 'male'
-donghun.blood_type = 'RH+ A'
+donghun = Person('이동훈',170,'male')  # 이렇게 만들어진 객체를 instance라 한다. # 객체 생성.
+# donghun.name = '이동훈'
+# donghun.height = '170'
+# donghun.gender = 'male'
+# donghun.blood_type = 'RH+ A'
 
 print(f'{donghun.name}의 혈액형은 {donghun.blood_type}입니다.')
 
 donghun.run('Fast')
+print(donghun)
+
+# 1. 초기화 후 객체생성
+hong = Person()
+hong.run('Slow')
+print(hong)
+
+print('==========================')
+# 2. 파라미터를 받는 생성자 실행
+ashely = Person('애슐리', 165, 'female')
+print(ashely.name)
+print(ashely.height)
+print(ashely.gender)
+
+print(ashely)
---
+++ Day04/code23_car.py
@@ -0,0 +1,42 @@
+
+
+# 자동차 클래스
+
+
+class Car:                      # 초록색은 클래스
+    __number = ' 54라 1234 '    # 캡슐화(encapsulaiton)
+
+    def get_number(self) -> str:
+        return self.__number
+
+    # 클래스 외부에선 변경X, 멤버함수로는 내부 조작O
+    def set_number(self, number):
+        self.__number = number
+    
+    def __init__(self, number = '54라 1234') -> None:
+        print('__init__')
+        self.__number = number
+
+    # def __new__(cls):               
+    #     print('__new__')
+    #     return super().__new__(cls)     # 부모클래스(상속) / super -> 부모
+    
+    def __str__(self) -> str:
+        return f'차번호는 {self.__number}입니다'
+
+yourcar = Car('88호 1234')
+print(yourcar)
+yourcar.__number = '11가1111'   # 외부에서는 멤버변수에 접근불가
+print(yourcar)
+
+print('클래스 내부함수 사용!')
+yourcar.set_number('22나2222')
+print(yourcar)
+
+mycar = Car()
+print(mycar)
+print(f'제 차는요, 아이오닉이고, 번호는 {mycar.get_number()}입니다.')
+
+mycar.__number = '123가 6789'
+print(mycar)
+print(mycar.get_number())
